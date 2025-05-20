a1415103eb13101d527233b3c26f48829000f8bf
DucAnh Pham <ducanh.p@outlook.com>
2025-05-20 23:51:50 +0200

    added ios updates

--- Framework/wearable_health/example/lib/services/metric_fetches/heart_rate.dart
+++ Framework/wearable_health/example/lib/services/metric_fetches/heart_rate.dart
@@ -5,6 +5,8 @@ import 'package:wearable_health/model/health_data.dart';
 import 'package:wearable_health/model/health_connect/enums/hc_health_metric.dart';
 import 'package:wearable_health/model/health_connect/hc_entities/heart_rate.dart';
 import 'package:wearable_health/extensions/open_m_health/health_connect/health_connect_heart_rate.dart';
+import 'package:wearable_health/extensions/open_m_health/health_kit/health-kit_heart_rate.dart';
+import 'package:wearable_health/model/health_kit/enums/hk_health_metric.dart';
 
 Future<List<dynamic>> fetchHeartRateData({
   required dynamic provider,
@@ -13,7 +15,20 @@ Future<List<dynamic>> fetchHeartRateData({
   required bool convert,
 }) async {
   final platformMetric = HealthConnectHealthMetric.heartRate;
-  if (!isAndroid) return []; // iOS support could be added similarly
+  if (!isAndroid) {
+    final platformMetric = HealthKitHealthMetric.heartRate;
+
+    if (convert) {
+      final data = await provider.getData([platformMetric], range);
+      // Replace this with your real HealthKit-to-OpenMHealth converter
+      return (data as List)
+          .expand((e) => e.toOpenMHealthHeartRate()) // You must implement this
+          .toList();
+    } else {
+      final HealthData raw = await provider.getRawData([platformMetric], range);
+      return [raw.data];
+    }
+  }
 
   if (convert) {
     final data = await provider.getData([platformMetric], range);
--- Framework/wearable_health/example/lib/services/metric_fetches/heart_rate_variability.dart
+++ Framework/wearable_health/example/lib/services/metric_fetches/heart_rate_variability.dart
@@ -5,6 +5,7 @@ import 'package:wearable_health/model/health_data.dart';
 import 'package:wearable_health/model/health_connect/enums/hc_health_metric.dart';
 import 'package:wearable_health/model/health_connect/hc_entities/heart_rate_variability_rmssd.dart';
 import 'package:wearable_health/extensions/open_m_health/health_connect/health_connect_heart_rate_variability.dart';
+import 'package:wearable_health/model/health_kit/enums/hk_health_metric.dart';
 
 Future<List<dynamic>> fetchHeartRateVariabilityData({
   required dynamic provider,
@@ -13,7 +14,19 @@ Future<List<dynamic>> fetchHeartRateVariabilityData({
   required bool convert,
 }) async {
   final platformMetric = HealthConnectHealthMetric.heartRateVariability;
-  if (!isAndroid) return [];
+  if (!isAndroid) {
+    final platformMetric = HealthKitHealthMetric.heartRateVariability;
+
+    if (convert) {
+      final data = await provider.getData([platformMetric], range);
+      return (data as List)
+          .expand((e) => e.toOpenMHealthHeartRateVariabilityRmssd()) // You must implement this
+          .toList();
+    } else {
+      final HealthData raw = await provider.getRawData([platformMetric], range);
+      return [raw.data];
+    }
+  }
 
   if (convert) {
     final data = await provider.getData([platformMetric], range);
